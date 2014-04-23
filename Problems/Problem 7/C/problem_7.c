4bf0c3f11dacb3de65f0686287927094cb2ce289
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-04-23 09:51:39 +0200

    [ Create ] Problem 7 C Procedure NOT FINISHED YET

---
+++ Problems/Problem 7/C/problem_7.c
@@ -0,0 +1,20 @@
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Problem 7 from Project Euler
+ *
+ * By listing the first six prime numbers: 2,3,5,7,11 and 13, we can see that the 6th prime is 13
+ * What is the 10001st prime number?
+ *
+ * PLEASE NOTE
+ * In order to compile this file you must have gcc, or similar, installed
+ *
+ * REGARDING THE PERFORMANCE
+ * This is not the final version of the algorithm, any suggestion or improvment will be very accepted.
+ * If you have a better solution please submit a pull request or an issue on Github.
+ *
+ * @author Claudio Ludovico Panetta (@Ludo237)
+ * @version 1.0.0
+ **/
