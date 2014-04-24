6467eb5390afa1530be805a59844932cc89822d6
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-04-24 11:13:49 +0200

    [ Create ] Problem 9 C Algorithm

---
+++ Problems/Problem 9/C/problem_9.c
@@ -0,0 +1,57 @@
+#include <time.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Problem 9 from Project Euler
+ *
+ * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+ * a^2 + b^2 = c^2
+ * 
+ * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc.
+ * 
+ * PLEASE NOTE
+ * In order to compile you must have gcc, or similar, installed
+ *
+ * REGARDING THE PERFORMANCE
+ * This is not the final version of the algorithm, any suggestion or improvment will be very accepted.
+ * If you have a better solution submit a pull request or ani issue on Github.
+ *
+ * @author Claudio Ludovico Panetta (@Ludo237)
+ * @version 1.0.0
+ **/
+
+#define END 1000 // You can change this
+
+int main(int argc, char* argv[])
+{
+    double start, end = 0;
+    printf("Algorithm starts... Please wait.\n");
+    start = clock(); // Benchmark starts
+    // Algorithm logic starts here
+    unsigned int a,b,c = 0;
+    // First Iteration
+    for(a = 1; a <= END; a++)
+    {
+        // Second iteration
+        for(b = (a+1); b <= (END - a); b++)
+        {
+            // Check that a + b + c == END.
+            c = (END - a - b); // Invert the formula, easy to assing c in this way
+            // Check if the power of a and b is equal to the power of c
+            if( ( pow(a,2) + pow(b,2) == pow(c,2) ) )
+            {
+                printf("Triplet found\n");
+                break;
+            }
+        }
+    }
+    // Algorithm end
+    end = clock();
+    printf("Algorithm terminated\n");
+    // Print benchmark
+    printf("Total Time: %f\n", (end - start)/CLOCKS_PER_SEC);
+    return 0;
+}
