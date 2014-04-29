c18ee58219896ba4e317ccec9ea09726ade4a4cc
Claudio Ludovico Panetta <ludovico1990@hotmail.it>
2014-04-29 09:10:52 +0200

    [ Create ] Problem 13 C Algorithm

---
+++ Problems/Problem 13/C/problem_13.c
@@ -0,0 +1,22 @@
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * Problem 13 from Project Euler
+ *
+ *  Work out the first ten digits of the sum of the following one-hundred 50-digit numbers.
+ *  ** See ../numbers.txt for the numbers **
+ *
+ *  PLEASE NOTE
+ *  In order to compile you must have gcc, or similar, installed
+ *
+ *  REGARDING THE PERFORMANCE
+ *  This is not the final version of the script, everyone can improve it. If you have a better solution
+ *  please submit a pull request or an issue on Github.
+ *
+ *  @author Claudio Ludovico Panetta (@Ludo237)
+ *
+ *  @version 1.0.0
+ **/
+#define DIGITS 50
+#define NUMBERS 100
