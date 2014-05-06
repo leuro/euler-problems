89c8c5975575b6f18133659b22af910e436108d3
Claudio Ludovico <ludovico1990@hotmail.it>
2014-05-06 15:08:02 +0200

    Update problem_25.c

--- Problems/Problem 25/C/problem_25.c
+++ Problems/Problem 25/C/problem_25.c
@@ -71,7 +71,6 @@ int main(int argc, char* argv[])
     }
     // Algorithm end
     end = clock();
-    printf("%d\n", total);
     printf("Algorithm end\n");
     printf("Total time: %f\n", (end - start)/CLOCKS_PER_SEC);
     return 0;
